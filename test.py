f4971f8afad5b9230d68e8e900920231b54635f9
МС ЧЕХНИК <petrichenko8502@gmail.com>
2025-01-18 20:57:26 +0400

    araray

--- test.py
+++ test.py
@@ -34,6 +34,56 @@
 #         return False
     
   
-# print(get_new_orders('cb98985a2930f87b9f5f0014127253f041321179'))
+# print(get_new_orders())
+
+from time import sleep
+import pyautogui
+import random
+from dataclasses import dataclass
 
 
+@dataclass
+class Region:
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+class Clicker:
+    def __init__(self, radius: int):
+        point = pyautogui.position()
+        x1, y1 = (int(point.x - radius), int(point.y - radius))
+        x2, y2 = (int(point.x + radius), int(point.y + radius))
+        self.region = Region(x1, y1, x2, y2)
+
+    def get_random_point_position(self) -> Point:
+        x = random.randint(self.region.x1, self.region.x2)
+        y = random.randint(self.region.y1, self.region.y2)
+
+        return Point(x, y)
+
+    def start(self, clicks: int):
+        counter = 0
+
+        while counter <= clicks:
+            point: Point = self.get_random_point_position()
+            pyautogui.click(x=point.x, y=point.y, button='left')
+            sleep(random.randint(1, 10) / 100)
+
+            counter += 1
+
+
+def main():
+    clicker = Clicker(radius=5)
+    clicker.start(clicks=100)
+
+
+if __name__ == '__main__':
+    main()
